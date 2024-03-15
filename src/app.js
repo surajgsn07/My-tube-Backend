331c6410593f2e5e9e75f7f4ef7342b9b97219e1
surajgsn07 <surajgn07@gmail.com>
2024-03-15 14:12:14 +0530

    hai kch

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://watch-it-surajgsn.netilify.app',
     credentials:true
 }))
 
--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import 'dotenv/config'
+// dotenv.config({})
+// dotenv()
+console.log(process.env.ACCESS_TOKEN_SECRET)
 
 const connectDB  = async ()=>{
     try {
+        console.log(process.env.MONGODB_URI)
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     
     console.log(`Mongodb connected with host ${connectionInstance.connection.host}`)
