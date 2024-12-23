55d4ee4e0b42ac69cbca19f89a977c46c0af88b0
surajgsn07 <120456653+surajgsn07@users.noreply.github.com>
2024-12-23 20:59:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000 , ()=>{
+    app.listen(process.env.PORT  , ()=>{
         console.log(`Server started at port ${process.env.PORT}`)
     }) 
 })
