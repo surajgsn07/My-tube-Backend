d07070fd53da6943e232940c694f6d5b6c46d245
surajgsn07 <120456653+surajgsn07@users.noreply.github.com>
2024-12-25 01:50:50 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -150,8 +150,6 @@ const loginUser = asyncHandler(async(req,res)=>{
 
     return res
             .status(200)
-            .cookie("accessToken",accessToken ,option)
-            .cookie("refreshToken" , refreshToken ,option)
             .json(
                 new ApiResponse(
                     200 ,
